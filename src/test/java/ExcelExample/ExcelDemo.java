968a8dd296efc1c851fcd166ee40c17c8d0dda55
Your Name <navyasree.tangudu@zenq.com>
2019-07-16 17:45:40 +0530

    files added

--- src/test/java/ExcelExample/ExcelDemo.java
+++ src/test/java/ExcelExample/ExcelDemo.java
@@ -19,7 +19,7 @@ import page.LoginPage;
 
 public class ExcelDemo {
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws IOException {
         WebDriver driver;
         String userName;
         String passWord;
@@ -34,7 +34,7 @@ public class ExcelDemo {
           driver = new ChromeDriver();
           driver.manage().window().maximize();
           driver.get("https://orangehrm-demo-6x.orangehrmlive.com/symfony/web/index.php/auth/login");
-        Thread.sleep(3000);
+
         LoginPage loginPage = new LoginPage(driver);
         userName = excelData.getRow(i).getCell(0).getStringCellValue();
         passWord = excelData.getRow(i).getCell(1).getStringCellValue();
